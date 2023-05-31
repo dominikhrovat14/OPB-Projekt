c9cf973abf14ddb891d636fc5b02d9d0819a07d5
MilkaGruber <64840327+MilkaGruber@users.noreply.github.com>
2023-05-31 16:50:26 +0200

    Spremenjene tabele

--- setup_db.py
+++ setup_db.py
@@ -5,7 +5,7 @@
 
 import psycopg2, psycopg2.extensions, psycopg2.extras
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s sumniki
-conn = psycopg2.connect(database='sem2023_dominikh', host='baza.fmf.uni-lj.si', user='dominikh', password='1pmlybto')
+conn = psycopg2.connect(database='sem2023_milkag', host='baza.fmf.uni-lj.si', user='milkag', password='ljhgej43')
 conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemogocimo transakcije
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
 # Nadaljni ukazi so enaki ne glede na to, ali "cur" prihaja iz sqlite ali psycopg2 knjiznice:
@@ -14,10 +14,10 @@ cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
                 #PRAVICE#
 
 
-cur.execute("GRANT ALL ON DATABASE sem2023_dominikh TO milkag;")
-cur.execute("GRANT ALL ON SCHEMA public TO milkag;")
-cur.execute("GRANT ALL ON ALL TABLES IN SCHEMA public TO milkag;")
-cur.execute("GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO milkag;")
+cur.execute("GRANT ALL ON DATABASE sem2023_milkag TO dominikh;")
+cur.execute("GRANT ALL ON SCHEMA public TO dominikh;")
+cur.execute("GRANT ALL ON ALL TABLES IN SCHEMA public TO dominikh;")
+cur.execute("GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO dominikh;")
 
 
 #cur.execute("GRANT SELECT, UPDATE, INSERT ON DATABASE sem2023_dominikh TO javnost;")
@@ -38,12 +38,19 @@ cur.execute("CREATE TABLE uporabnik ( "
       + " username TEXT NOT NULL UNIQUE, "
       + " geslo TEXT NOT NULL, "
       + " email TEXT NOT NULL UNIQUE, "
-      + " starost INTEGER); ")
+      + " starost INTEGER, "
+      + " kraj TEXT NOT NULL, "
+      + " ulica TEXT NOT NULL, "
+      + " hisna INTEGER NOT NULL, "
+      + " posta INTEGER NOT NULL); "
+      )
+
 
 # Kreiranje tabele knjiga
 cur.execute("CREATE TABLE knjiga ( "
       + " id SERIAL PRIMARY KEY, "
       + " naslov TEXT NOT NULL, "
+      + " avtor TEXT NOT NULL, "
       + " ocena DECIMAL NOT NULL DEFAULT 0, "
       + " stevilo_ocen INTEGER NOT NULL DEFAULT 0, "
       + " leto_izdaje INTEGER NOT NULL, "
@@ -51,13 +58,18 @@ cur.execute("CREATE TABLE knjiga ( "
       + " zanr TEXT NOT NULL, "
       + " jezik TEXT NOT NULL); ")
 
-# kreiranje tabele izposoja
+# kreiranje tabele opisi
+cur.execute("CREATE TABLE opisi ( "
+      + " naslov TEXT NOT NULL, "
+      + " opis TEXT NOT NULL); ")
 
+# kreiranje tabele izposoja
 cur.execute("CREATE TABLE izposoja ( "
       + " id SERIAL PRIMARY KEY, "
       + " id_knjige INTEGER NOT NULL, "
       + " id_uporabnika INTEGER NOT NULL, "
       + " datum_izposoje DATE NOT NULL, "
       + " datum_vracila DATE NOT NULL); ")
+
 
 print("KONČANO BREZ NAPAK")
